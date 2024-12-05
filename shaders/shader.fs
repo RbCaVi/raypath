52337cdab07605aec89e0c48b147ad670cc1e54b
RbCaViTerminal <robert@robertvail.info>
2024-12-04 22:03:16 -0500

    there are now two planes

--- shaders/shader.fs
+++ shaders/shader.fs
@@ -10,12 +10,21 @@ float rand(vec2 v) {
     return fract(sin(dot(v ,vec2(12.9898,78.233))) * 43758.5453) - 0.5;
 }
 
-vec3 get_color(vec3 raypos, vec3 raydir) {
+vec3 bg() {
+    return vec3(vec2(0.5, 0.5) + pos * vec2(0.5, -0.5), mod(time, 1.0));
+}
+
+struct hit {
+    float dist;
+    vec3 color;
+};
+
+hit raytrace(vec3 raypos, vec3 raydir) {
 
     // plane intersection
     vec3 planecenter = vec3(0.0, 0.0, 3.0);
     vec3 planev1 = vec3(1.0, 0.0, 0.0);
-    vec3 planev2 = vec3(0.0, 1.0, 0.0);
+    vec3 planev2 = vec3(1.0, 1.0, 0.0);
 
     vec3 n = cross(planev1, planev2);
     vec3 normal = normalize(n);
@@ -25,13 +34,13 @@ vec3 get_color(vec3 raypos, vec3 raydir) {
     float denom = dot(normal, raydir);
 
     if (abs(denom) < 1e-8) {
-        return vec3(vec2(0.5, 0.5) + pos * vec2(0.5, -0.5), mod(time, 1.0));
+        return hit(-1, vec3(0.0));
     }
 
     float t = (D - dot(normal, raypos)) / denom;
 
     if (t < 0) {
-        return vec3(vec2(0.5, 0.5) + pos * vec2(0.5, -0.5), mod(time, 1.0));
+        return hit(-1, vec3(0.0));
     }
 
     vec3 intersection = raypos + raydir * t;
@@ -42,7 +51,62 @@ vec3 get_color(vec3 raypos, vec3 raydir) {
     float a = dot(w, cross(P, planev2));
     float b = dot(w, cross(planev1, P));
 
-    return vec3(mod(a, 1.0), mod(b, 1.0), 1-mod(time, 1.0));
+    return hit(t, vec3(1-mod(time, 1.0), mod(a, 1.0), mod(b, 1.0)));
+}
+
+hit raytrace2(vec3 raypos, vec3 raydir) {
+
+    // plane intersection
+    vec3 planecenter = vec3(3.0, 3.0, 3.0);
+    vec3 planev1 = vec3(0.0, -1.0, 1.0);
+    vec3 planev2 = vec3(1.0, 0.0, 0.0);
+
+    vec3 n = cross(planev1, planev2);
+    vec3 normal = normalize(n);
+    vec3 Q = planecenter;
+    float D = dot(normal, Q);
+
+    float denom = dot(normal, raydir);
+
+    if (abs(denom) < 1e-8) {
+        return hit(-1, vec3(0.0));
+    }
+
+    float t = (D - dot(normal, raypos)) / denom;
+
+    if (t < 0) {
+        return hit(-1, vec3(0.0));
+    }
+
+    vec3 intersection = raypos + raydir * t;
+    vec3 w = n / dot(n,n);
+
+    vec3 P = intersection;
+
+    float a = dot(w, cross(P, planev2));
+    float b = dot(w, cross(planev1, P));
+
+    return hit(t, vec3(1-mod(time, 1.0), mod(a, 1.0), mod(b, 1.0)));
+}
+
+vec3 get_color(hit p) {
+    if (p.dist < 0.0) {
+        return bg();
+    }
+    return p.color;
+}
+
+hit merge_hits(hit h1, hit h2) {
+    if (h1.dist < 0) {
+        return h2;
+    }
+    if (h2.dist < 0) {
+        return h1;
+    }
+    if (h2.dist < h1.dist) {
+        return h2;
+    }
+    return h1;
 }
 
 void main()
@@ -52,10 +116,10 @@ void main()
 
     vec3 color = vec3(0.0);
 
-    int samples = 5;
+    int samples = 10;
     float invsamples = 1.0 / samples;
     for(int i = 0; i < samples; i++) {
-        vec2 tpos = pos + vec2(rand(pos + vec2(time, i)), rand(pos + vec2(time + 1, i * 2))) / vec2(width, height) * 2;
+        vec2 tpos = pos + vec2(rand(pos + vec2(time, i)), rand(pos + vec2(time + 1, i * 2))) / vec2(width, height) * 4;
         vec3 raydir = vec3(tpos, 1.0) * mat3(
             cos(time), sin(time), 0.0,
             -sin(time), cos(time), 0.0,
@@ -65,7 +129,7 @@ void main()
             0.0, 1.0, 0.0,
             -sin(pi/3), 0.0, cos(pi/3)
         );
-        color += get_color(raypos, raydir);
+        color += get_color(merge_hits(raytrace(raypos, raydir), raytrace2(raypos, raydir)));
     }
 
     gl_FragColor = vec4(color * invsamples, 0.5);
