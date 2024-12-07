57f945d8314e21befa9a6d28da842d4d9de3d835
RbCaViTerminal <robert@robertvail.info>
2024-12-07 00:00:41 -0500

    add a second sphere (reflective, "needs" recursion (i could actually make it iterative))

--- shaders/shader.fs
+++ shaders/shader.fs
@@ -157,6 +157,71 @@ float map(float value, float min1, float max1, float min2, float max2) {
   return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
 }
 
+vec3 castray2(ray r) {
+    return get_color(
+        merge_hits(
+            merge_hits(
+                pathtrace(r),
+                pathtrace2(r)
+            ),
+            pathtrace3(r)
+        )
+    );
+}
+
+hit pathtrace4(ray r) {
+    vec3 center = vec3(2.0, 0.0, 3.0);
+    float radius = 0.5;
+    
+    vec3 oc = center - r.pos;
+    float a = dot(r.dir, r.dir);
+    float h = dot(r.dir, oc);
+    float c = dot(oc, oc) - radius*radius;
+    float d = h*h - a*c;
+
+    if (d < 0) {
+        return hit(-1.0, vec3(0.0));
+    }
+
+    float ainv = 1.0 / a;
+    float sqrtd_a = sqrt(d) * ainv;
+    float h_a = h * ainv;
+
+    if (h_a < -sqrtd_a) {
+        // neither the h - sqrtd nor the h + sqrtd solutions will work
+        return hit(-1.0, vec3(0.0));
+    }
+
+    float t;
+    if (h_a < sqrtd_a) {
+        // the "close" solution is behind the ray origin
+        t = h_a + sqrtd_a;
+    } else {
+        t = h_a - sqrtd_a;
+    }
+
+    vec3 normal = (center - at(r, t)) / radius;
+
+    vec3 ref = r.dir - 2 * dot(r.dir, normal) * normal;
+
+    return hit(t, castray2(ray(at(r, t), ref)));
+}
+
+vec3 castray(ray r) {
+    return get_color(
+        merge_hits(
+            merge_hits(
+                pathtrace(r),
+                pathtrace2(r)
+            ),
+            merge_hits(
+                pathtrace3(r),
+                pathtrace4(r)
+            )
+        )
+    );
+}
+
 void main()
 {
     if (pos.x > 0.9) {
@@ -193,7 +258,7 @@ void main()
         vec2 tpos = pos + vec2(rand(pos + vec2(time, i)), rand(pos + vec2(time + 1, i * 2))) / vec2(width, height) * 4; // * 2 makes less blurring but maybe more aliasing? (not sure)
         vec3 raydir = vec3(tpos, 1.0) * camera;
         ray r = ray(campos, raydir);
-        color += get_color(merge_hits(pathtrace(r), merge_hits(pathtrace2(r), pathtrace3(r))));
+        color += castray(r);
     }
 
     gl_FragColor = vec4(color * invsamples, 1.0);
